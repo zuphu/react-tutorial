e8b16020ea1e51be0663b2442dadea790b12b8ae
Anthony Guevara (scorpionix) <anthony.guev@gmail.com>
2017-07-20 22:58:12 -0400

    Alternate between X and O.

--- src/index.js
+++ src/index.js
@@ -30,12 +30,15 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player:' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
