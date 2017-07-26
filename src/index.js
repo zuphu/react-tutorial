9ca42a72213e9f1c923568c765273e2cf8027ae3
Anthony Guevara (scorpionix) <anthony.guev@gmail.com>
2017-07-25 23:39:38 -0400

    Add history to Game state.

--- src/index.js
+++ src/index.js
@@ -74,6 +74,15 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null),
+      }],
+      isNext: true,
+    };
+  }
   render() {
     return (
       <div className="game">
