c897773decdf93e62935c5678a4ccde79dc53489
Anthony Guevara (scorpionix) <anthony.guev@gmail.com>
2017-07-26 22:57:58 -0400

    Update rendersquare function.

--- src/index.js
+++ src/index.js
@@ -19,12 +19,8 @@ class Board extends React.Component {
     }
   }
   renderSquare(i) {
-    return <Square value={
-      this.state.squares[i]
-    }
-    onClick={
-      () => this.handleClick(i)
-    }
+    return <Square value={this.state.squares[i]}
+    onClick={() => this.props.onClick(i)}
     />
   }
 
