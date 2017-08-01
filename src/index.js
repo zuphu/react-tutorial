190b740230a0199f6274295c7e3bdceb4322008c
Anthony Guevara (scorpionix) <anthony.guev@gmail.com>
2017-07-31 23:52:32 -0400

    Update game board render function.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+/* Anthony */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -82,20 +84,20 @@ class Game extends React.Component {
     };
   }
   render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-         <Board
-            squares={current.squares}
-            onClick={(i) => this.handleClick(i)}
-          />
-        </div>
-        <div className="game-info">
-          <div>{status}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Move #' + move :
+        'Game start';
+      return (
+        <li>
+          <a href="#" onClick={() => this.jumpTo(move)}>{desc}</a>
+        </li>
+      );
+    });
   }
 }
 
