9e71019097988f6c28cc87bd49518c725f7bfd5d
Anthony Guevara (scorpionix) <anthony.guev@gmail.com>
2017-08-01 23:59:24 -0400

    Update game render function.

--- src/index.js
+++ src/index.js
@@ -92,11 +92,43 @@ class Game extends React.Component {
       const desc = move ?
         'Move #' + move :
         'Game start';
+  render() {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Move #' + move :
+        'Game start';
       return (
         <li>
           <a href="#" onClick={() => this.jumpTo(move)}>{desc}</a>
         </li>
       );
+    });
+
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
+        </div>
+        <div className="game-info">
+          <div>{status}</div>
+          <ol>{moves}</ol>
+        </div>
+      </div>
+    );
     });
   }
 }
